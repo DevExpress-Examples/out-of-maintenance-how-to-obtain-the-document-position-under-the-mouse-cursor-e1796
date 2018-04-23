8504fd88fd2b048b35eb474c1d3daff10bc8bbaf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/GetPositionFromPoint/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Resources;
+using DevExpress.XtraRichEdit;
+using DevExpress.XtraRichEdit.API.Native;
+using DevExpress.XtraRichEdit.Utils;
+
+namespace GetPositionFromPoint
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            richEdit1.ApplyTemplate();
+            richEdit1.Loaded += richEdit1_Loaded;
+        }
+
+        #region #mouseclick
+        private void richEdit1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Point point = e.GetPosition((UIElement)richEdit1.RichControl.Parent);
+            System.Drawing.Point pt = Units.PixelsToDocuments(new System.Drawing.Point((int)point.X,
+        (int)point.Y), richEdit1.RichControl.DpiX, richEdit1.RichControl.DpiY);
+            
+            DocumentPosition pos = richEdit1.RichControl.GetPositionFromPoint(pt);
+            if (pos != null)
+            {
+                DocumentRange range = richEdit1.RichControl.Document.CreateRange(pos, 1);
+                tbClickedLetter.Text = richEdit1.RichControl.Document.GetText(range);
+            }
+        }
+        #endregion #mouseclick
+        static Stream GetStream(string filename)
+        {
+            StreamResourceInfo sr = Application.GetResourceStream(new Uri(filename, UriKind.Relative));
+            return sr.Stream;
+        }
+
+        private void richEdit1_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (richEdit1.RichControl != null)
+            {
+                richEdit1.RichControl.LoadDocument(GetStream("sample.rtf"), DocumentFormat.Rtf);
+                //richEdit1.RichControl.LayoutUnit = DocumentLayoutUnit.Pixel;
+                richEdit1.RichControl.Unit = DocumentUnit.Document;
+                richEdit1.RichControl.FocusKeyCodeTextBox();
+            }
+        }
+
+       
+
+
+    }
+}
---
+++ VB/GetPositionFromPoint/MainPage.xaml.vb
@@ -0,0 +1,51 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.IO
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Input
+Imports System.Windows.Resources
+Imports DevExpress.XtraRichEdit
+Imports DevExpress.XtraRichEdit.API.Native
+Imports DevExpress.XtraRichEdit.Utils
+
+Namespace GetPositionFromPoint
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			richEdit1.ApplyTemplate()
+			AddHandler richEdit1.Loaded, AddressOf richEdit1_Loaded
+		End Sub
+
+		#Region "#mouseclick"
+		Private Sub richEdit1_MouseLeftButtonUp(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+			Dim point As Point = e.GetPosition(CType(richEdit1.RichControl.Parent, UIElement))
+			Dim pt As System.Drawing.Point = Units.PixelsToDocuments(New System.Drawing.Point(CInt(Fix(point.X)), CInt(Fix(point.Y))), richEdit1.RichControl.DpiX, richEdit1.RichControl.DpiY)
+
+			Dim pos As DocumentPosition = richEdit1.RichControl.GetPositionFromPoint(pt)
+			If pos IsNot Nothing Then
+				Dim range As DocumentRange = richEdit1.RichControl.Document.CreateRange(pos, 1)
+				tbClickedLetter.Text = richEdit1.RichControl.Document.GetText(range)
+			End If
+		End Sub
+		#End Region ' #mouseclick
+		Private Shared Function GetStream(ByVal filename As String) As Stream
+			Dim sr As StreamResourceInfo = Application.GetResourceStream(New Uri(filename, UriKind.Relative))
+			Return sr.Stream
+		End Function
+
+		Private Sub richEdit1_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			If richEdit1.RichControl IsNot Nothing Then
+				richEdit1.RichControl.LoadDocument(GetStream("sample.rtf"), DocumentFormat.Rtf)
+				'richEdit1.RichControl.LayoutUnit = DocumentLayoutUnit.Pixel;
+				richEdit1.RichControl.Unit = DocumentUnit.Document
+				richEdit1.RichControl.FocusKeyCodeTextBox()
+			End If
+		End Sub
+
+
+
+
+	End Class
+End Namespace
